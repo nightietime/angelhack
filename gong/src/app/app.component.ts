1143be278e34827707e34c4caad5ce75482824b7
krusli <rusli.kenneth@gmail.com>
2017-05-07 05:27:26 +1000

    Update template

--- gong/src/app/app.component.ts
+++ gong/src/app/app.component.ts
@@ -4,7 +4,10 @@ import { ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'body',
   // template: `<search-form></search-form>`,
-  template: `<router-outlet></router-outlet>`,
+  template: `<router-outlet></router-outlet>
+  <!-- Include Javascript and Jquery -->
+  <script src="https://code.jquery.com/jquery-3.1.1.js" integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA=" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>`,
   encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent  {
